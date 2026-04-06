639d0abb44a1049cd8611da2d4e3309902ccfee8
Balazs Beregnyei <balazs.beregnyei@gmail.com>
2026-04-06 19:58:56 +0200

    make.bat wrapper fix 2

--- make.bat
+++ make.bat
@@ -1,4 +1,5 @@
 @echo off
 :: make.bat — Run GNU make via MSYS2 UCRT64 environment
 :: Usage: make.bat [targets...] (same syntax as make)
+set "MSYSTEM=UCRT64"
 C:\msys64\usr\bin\bash.exe -lc "cd '%cd:\=/%' && make %*"
